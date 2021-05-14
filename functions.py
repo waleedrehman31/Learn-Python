0a3e7f310ef46b035831cb3fc4ca1083c1dabc03
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-05-14 15:22:16 +0500

    first commit

--- functions.py
+++ functions.py
@@ -1,6 +1,50 @@
 # python functions
 
-def greet(name):
-    print(f"Hello {name} how are you")
+# python function with passing an arguments
 
-greet("waleed")
+def greet(name, age=-1):
+    print(f"Hello {name} how are you")
+    if age >= 0:
+        print(f"I know your age is {age}")
+
+
+# greet("waleed", 20)
+# greet("Hameed", 10)
+
+
+# Function with return value
+# def anAdult(age):
+#     if age >= 16:
+#         return True
+#     else:
+#         return False
+#
+# result = anAdult(45)
+# print(result)
+
+#! OR
+
+def anAdult(age):
+    return age >= 16
+
+result = anAdult(45)
+# print(result)
+
+def convertGender(gender="unknown"):
+    if gender.upper() == "M":
+        return  "Male"
+    elif gender.upper() == "F":
+        return  "Female"
+    else:
+        return f"Gender: {gender} is unknown"
+
+# print(convertGender("M"))
+# print(convertGender("F"))
+# print(convertGender("m"))
+# print(convertGender("f"))
+# print(convertGender("Hello Gender"))
+
+#  Built in functions and Import Statement
+
+import math
+print(math.isqrt(25))
